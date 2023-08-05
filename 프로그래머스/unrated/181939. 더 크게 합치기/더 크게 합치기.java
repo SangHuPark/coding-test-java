4047785690b7f4e146a88c4e18c2a4c66ca6ac38
SangHuPark <tkdgn5408407@gmail.com>
2023-08-05 16:36:58 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.05 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,25 +1,17 @@
 class Solution {
     public int solution(int a, int b) {
-        /*
-        int n = 0;
         
-        if ( Integer.parseInt(new StringBuilder()
-                                    .append(a).append(b).toString())
-           >= Integer.parseInt(new StringBuilder()
-                                    .append(b).append(a).toString()) )
-            n = Integer.parseInt(new StringBuilder()
+        int strSum1 = Integer.parseInt(new StringBuilder()
                                     .append(a).append(b).toString());
-        else
-            n = Integer.parseInt(new StringBuilder()
+        int strSum2 = Integer.parseInt(new StringBuilder()
                                     .append(b).append(a).toString());
-        
-        return n;
-        */
+
+        /*
         String strA = String.valueOf(a);
         String strB = String.valueOf(b);
         String strSum1 = strA + strB;
         String strSum2 = strB + strA;
+        */
         return Math.max(Integer.valueOf(strSum1), Integer.valueOf(strSum2));
-        
     }
 }
