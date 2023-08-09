d8df390f4f91f36e1509c76b09c16b39537f4abe
SangHuPark <tkdgn5408407@gmail.com>
2023-08-09 12:19:14 +0900

    [unrated] Title: 수 조작하기 2, Time: 627.18 ms, Memory: 393 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,23 +1,26 @@
 class Solution {
     public String solution(int[] numLog) {
         String str = new String();
+        int num = 0;
         
         for (int i = 1; i < numLog.length; i++) {
-            if (numLog[i-1] + 1 == numLog[i])
-                str += "w";
-            else if (numLog[i-1] - 1 == numLog[i])
-                str += "s";
-            else if (numLog[i-1] + 10 == numLog[i])
-                str += "d";
-            else if (numLog[i-1] - 10 == numLog[i])
-                str += "a";
-            // switch(numLog[i]) {
-            //         case(numLog[i-1] + 1) : str += "w"; break;
-            //         case(numLog[i-1] - 1) : str += "s"; break;
-            //         case(numLog[i-1] + 10) : str += "d"; break;
-            //         case(numLog[i-1] - 10) : str += "a"; break;
-            //         default : break;
-            // }
+            num = numLog[i] - numLog[i-1];
+            
+            // if (numLog[i-1] + 1 == numLog[i])
+            //     str += "w";
+            // else if (numLog[i-1] - 1 == numLog[i])
+            //     str += "s";
+            // else if (numLog[i-1] + 10 == numLog[i])
+            //     str += "d";
+            // else if (numLog[i-1] - 10 == numLog[i])
+            //     str += "a";
+            switch(num) {
+                    case(1) : str += "w"; break;
+                    case(-1) : str += "s"; break;
+                    case(10) : str += "d"; break;
+                    case(-10) : str += "a"; break;
+                    default : break;
+            }
         }
         
         return str;
