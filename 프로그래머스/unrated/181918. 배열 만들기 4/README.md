9a2ba37873db0496f34b21842d9945761af31f7f
SangHuPark <tkdgn5408407@gmail.com>
2023-08-11 16:24:29 +0900

    [unrated] Title: 배열 만들기 4, Time: 19.06 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/README.md
+++ 프로그래머스/unrated/181918. 배열 만들기 4/README.md
@@ -1,10 +1,10 @@
 # [unrated] 배열 만들기 4 - 181918 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181918) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181918?language=java) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 6.66 ms
+메모리: 76.3 MB, 시간: 19.06 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,8 +1,8 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        ArrayList<Integer> list = new ArrayList<>();
+        /* ArrayList<Integer> list = new ArrayList<>();
         int i = 0;
         
         while(i < arr.length) {
@@ -21,6 +21,21 @@ class Solution {
         }
             
         int[] stk = list.stream().mapToInt(j->j).toArray();
+        return stk; */
+        
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+
+        while (i < arr.length) {
+            if (stack.empty() || stack.peek() < arr[i]) {
+                stack.push(arr[i]);
+                i++;
+            } else if (stack.peek() >= arr[i]) {
+                stack.pop();
+            }
+        }
+        
+        int[] stk = stack.stream().mapToInt(j->j).toArray();
         return stk;
     }
 }
