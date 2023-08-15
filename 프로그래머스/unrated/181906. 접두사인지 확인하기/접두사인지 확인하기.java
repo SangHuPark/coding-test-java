350edc5f9cb7505afff23d41db1ea7bc66b939cf
SangHuPark <tkdgn5408407@gmail.com>
2023-08-15 15:20:56 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,19 +1,21 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        int res = 0;
-        String[] strArr = new String[my_string.length()];
+//         int res = 0;
+//         String[] strArr = new String[my_string.length()];
         
-        for (int i = 0; i < my_string.length(); i++) {
-            strArr[i] = my_string.substring(0, i);
-        }
+//         for (int i = 0; i < my_string.length(); i++) {
+//             strArr[i] = my_string.substring(0, i);
+//         }
         
-        for (int j = 0; j < my_string.length(); j++) {
-            if (strArr[j].equals(is_prefix)) {
-                res = 1;
-                break;
-            }
-        }
+//         for (int j = 0; j < my_string.length(); j++) {
+//             if (strArr[j].equals(is_prefix)) {
+//                 res = 1;
+//                 break;
+//             }
+//         }
         
-        return res;
+//         return res;
+        
+        return my_string.startsWith(is_prefix) ? 1 : 0;
     }
 }
