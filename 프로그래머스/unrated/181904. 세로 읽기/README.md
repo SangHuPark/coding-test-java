0382da49574dc1a80980c15c1d71e03dc1187ea6
SangHuPark <tkdgn5408407@gmail.com>
2023-08-15 17:48:36 +0900

    [unrated] Title: 세로 읽기, Time: 10.85 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/README.md
+++ 프로그래머스/unrated/181904. 세로 읽기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 세로 읽기 - 181904 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181904) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181904?language=java) 
 
 ### 성능 요약
 
-메모리: 69 MB, 시간: 10.18 ms
+메모리: 80.8 MB, 시간: 10.85 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -1,18 +1,20 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
-        // String[] strArr = new String[my_string.length() / m];
-        char[] ch = my_string.toCharArray();
-        String res = new String();
+//         char[] ch = my_string.toCharArray();
+//         String res = new String();
         
-        for (int i = 0; i < my_string.length() / m; i++) {
-            res += ch[i * m + c - 1];
+//         for (int i = 0; i < my_string.length() / m; i++) {
+//             res += ch[i * m + c - 1];
+//         }
+        
+//         return res;
+        
+        String ans = "";
+
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            ans += my_string.charAt(i);
         }
         
-        // for (int j = 0; j < my_string.length() / m; j++) {
-        //     char[] chArr = strArr[j].toCharArray();
-        //     res += Character.toString(chArr[c]);
-        // }
-        
-        return res;
+        return ans;
     }
 }
