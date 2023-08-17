4333b6d569d6a1c8d47edc328e889090580539f0
SangHuPark <tkdgn5408407@gmail.com>
2023-08-17 16:56:15 +0900

    [unrated] Title: 글자 지우기, Time: 1.48 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -2,19 +2,31 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string, int[] indices) {
+        // String answer = "";
+        // StringBuilder sb = new StringBuilder();
+        // boolean[] isDeleted = new boolean[my_string.length()];
+        // for(int index : indices){
+        //     isDeleted[index] = true;
+        // }
+        // for(int i = 0 ; i < my_string.length() ; i++){
+        //     if(isDeleted[i]){
+        //         continue;
+        //     }
+        //     sb.append(my_string.charAt(i));
+        // }
+        // answer = sb.toString();
+        // return answer;
+        
         String answer = "";
-        StringBuilder sb = new StringBuilder();
-        boolean[] isDeleted = new boolean[my_string.length()];
-        for(int index : indices){
-            isDeleted[index] = true;
+        String[] tmp = my_string.split("");
+
+        for (int i = 0; i < indices.length; i++) {
+            tmp[indices[i]] = "";
         }
-        for(int i = 0 ; i < my_string.length() ; i++){
-            if(isDeleted[i]){
-                continue;
-            }
-            sb.append(my_string.charAt(i));
+
+        for (String x : tmp) {
+            answer += x;
         }
-        answer = sb.toString();
         return answer;
     }
 }
