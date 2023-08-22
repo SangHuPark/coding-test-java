e1008014a87c2afdc0b2777a40137a0e7f1510b5
SangHuPark <tkdgn5408407@gmail.com>
2023-08-22 14:59:43 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.20 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/README.md
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/README.md
@@ -1,10 +1,10 @@
 # [unrated] n개 간격의 원소들 - 181888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181888) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181888?language=java) 
 
 ### 성능 요약
 
-메모리: 88 MB, 시간: 25.67 ms
+메모리: 74.3 MB, 시간: 0.20 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -2,13 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        ArrayList<Integer> list = new ArrayList<>();
+//         ArrayList<Integer> list = new ArrayList<>();
         
-        for(int i = 0; i < num_list.length; i+=n) {
-            list.add(num_list[i]);
+//         for(int i = 0; i < num_list.length; i+=n) {
+//             list.add(num_list[i]);
+//         }
+        
+//         int[] answer = list.stream().mapToInt(i->i).toArray();
+//         return answer;
+        
+        Double length = Math.ceil(num_list.length/(n*1.0));
+        int[] answer = new int[length.intValue()];
+
+        for(int idx=0; idx<length; idx++) {
+            answer[idx] = num_list[idx*n];
         }
-        
-        int[] answer = list.stream().mapToInt(i->i).toArray();
+
         return answer;
     }
 }
