d627d18b1202898da41f8cadc48b01542ef2981f
SangHuPark <tkdgn5408407@gmail.com>
2023-08-22 16:00:48 +0900

    [unrated] Title: 할 일 목록, Time: 12.70 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/README.md
+++ 프로그래머스/unrated/181885. 할 일 목록/README.md
@@ -1,10 +1,10 @@
 # [unrated] 할 일 목록 - 181885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181885?language=java) 
 
 ### 성능 요약
 
-메모리: 71.9 MB, 시간: 0.02 ms
+메모리: 78.4 MB, 시간: 12.70 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -1,15 +1,22 @@
 import java.util.*;
 
 class Solution {
-    public ArrayList<String> solution(String[] todo_list, boolean[] finished) {
-        ArrayList<String> list = new ArrayList<>();
+    public String[] solution(String[] todo_list, boolean[] finished) {
+//         ArrayList<String> list = new ArrayList<>();
         
-        for (int i = 0; i < todo_list.length; i++) {
-            if (!finished[i]) {
-                list.add(todo_list[i]);
-            }
+//         for (int i = 0; i < todo_list.length; i++) {
+//             if (!finished[i]) {
+//                 list.add(todo_list[i]);
+//             }
+//         }
+        
+//         return list;
+        
+        String str = "";
+        for(int i = 0; i < finished.length; i++){
+            str = finished[i] == false ? str + todo_list[i] + "," : str;
         }
-        
-        return list;
+
+        return str.split(",");
     }
 }
