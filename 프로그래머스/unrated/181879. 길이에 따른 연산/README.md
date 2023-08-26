f365c60394bc3f7933a2d489cfe3604481fc9b26
SangHuPark <tkdgn5408407@gmail.com>
2023-08-26 14:12:55 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/README.md
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/README.md
@@ -1,10 +1,10 @@
 # [unrated] 길이에 따른 연산 - 181879 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181879) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181879?language=java) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 0.01 ms
+메모리: 73.7 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -1,12 +1,23 @@
 class Solution {
     public int solution(int[] num_list) {
-        int temp = num_list[0];
+//         int temp = num_list[0];
         
-        if (num_list.length > 10) {
-            for (int i = 1; i < num_list.length; i++)
+//         if (num_list.length > 10) {
+//             for (int i = 1; i < num_list.length; i++)
+//                 temp += num_list[i];
+//         } else {
+//             for (int i = 1; i < num_list.length; i++)
+//                 temp *= num_list[i];
+//         }
+        
+//         return temp;
+        
+        int temp = num_list.length > 10 ? 0 : 1;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list.length > 10)
                 temp += num_list[i];
-        } else {
-            for (int i = 1; i < num_list.length; i++)
+            else
                 temp *= num_list[i];
         }
         
