944846a32c2ba14d290358f78f34dd76e69b28f3
SangHuPark <tkdgn5408407@gmail.com>
2023-09-01 14:39:50 +0900

    [unrated] Title: x 사이의 개수, Time: 5.94 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
@@ -1,18 +1,22 @@
 import java.util.*;
 
 class Solution {
-    public ArrayList<Integer> solution(String myString) {
-        ArrayList<Integer> list = new ArrayList<>();
-        String[] strArr = myString.split("x");
+    public int[] solution(String myString) {
+//         ArrayList<Integer> list = new ArrayList<>();
+//         String[] strArr = myString.split("x");
         
-        for (int i = 0; i < strArr.length; i++) {
-            list.add(strArr[i].length());
-        }
+//         for (int i = 0; i < strArr.length; i++) {
+//             list.add(strArr[i].length());
+//         }
         
-        if (myString.substring(myString.length() - 1).equals("x")) {
-            list.add(0);
-        }
+//         if (myString.substring(myString.length() - 1).equals("x")) {
+//             list.add(0);
+//         }
         
-        return list;
+//         return list;
+        
+        return Arrays.stream(myString.split("x", myString.length()))
+            .mapToInt(x -> x.length())
+            .toArray();
     }
 }
