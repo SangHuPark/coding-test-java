e4d0bff1eb25d3c878111a9eea26a6eb80141a72
SangHuPark <tkdgn5408407@gmail.com>
2023-09-03 22:35:57 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.07 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/README.md
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 문자열 바꿔서 찾기 - 181864 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181864) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181864?language=java) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 1.75 ms
+메모리: 70.9 MB, 시간: 0.07 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,21 +1,25 @@
 class Solution {
     public int solution(String myString, String pat) {
-        char[] arr = myString.toCharArray();
-        String str = new String();
+//         char[] arr = myString.toCharArray();
+//         String str = new String();
         
-        for (char ch : arr) {
-            switch (ch) {
-                case ('A'):
-                    str += "B";
-                    break;
-                case ('B'):
-                    str += "A";
-                    break;
-                default:
-                    return 0;
-            }
-        }
+//         for (char ch : arr) {
+//             switch (ch) {
+//                 case ('A'):
+//                     str += "B";
+//                     break;
+//                 case ('B'):
+//                     str += "A";
+//                     break;
+//                 default:
+//                     return 0;
+//             }
+//         }
         
-        return str.contains(pat) ? 1 : 0;
+//         return str.contains(pat) ? 1 : 0;
+        
+        myString = myString.replace("A", "a").replace("B", "A")
+            .replace("a", "B");
+        return myString.contains(pat) ? 1 : 0;
     }
 }
