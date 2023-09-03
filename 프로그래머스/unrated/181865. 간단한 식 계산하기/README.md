85d3ea2ee0d33ac8ca141141e2987a5f80f65833
SangHuPark <tkdgn5408407@gmail.com>
2023-09-03 22:28:07 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.09 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/README.md
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 간단한 식 계산하기 - 181865 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181865) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181865?language=java) 
 
 ### 성능 요약
 
-메모리: 74.2 MB, 시간: 0.14 ms
+메모리: 73.7 MB, 시간: 0.09 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -2,14 +2,30 @@ class Solution {
     public int solution(String binomial) {
         int ans = 0;
         String[] strArr = binomial.split(" ");
+        int num1 = Integer.valueOf(strArr[0]);
+        int num2 = Integer.valueOf(strArr[2]);
         
-        if (strArr[1].equals("+"))
-            ans = Integer.valueOf(strArr[0]) + Integer.valueOf(strArr[2]);
-        else if (strArr[1].equals("-"))
-            ans = Integer.valueOf(strArr[0]) - Integer.valueOf(strArr[2]);
-        else if (strArr[1].equals("*"))
-            ans = Integer.valueOf(strArr[0]) * Integer.valueOf(strArr[2]);
+        // if (strArr[1].equals("+"))
+        //     ans = Integer.valueOf(strArr[0]) + Integer.valueOf(strArr[2]);
+        // else if (strArr[1].equals("-"))
+        //     ans = Integer.valueOf(strArr[0]) - Integer.valueOf(strArr[2]);
+        // else if (strArr[1].equals("*"))
+        //     ans = Integer.valueOf(strArr[0]) * Integer.valueOf(strArr[2]);
         
+        char op = strArr[1].charAt(0);
+        switch (op) {
+            case '+':
+                ans = num1 + num2;
+                break;
+            case '-':
+                ans = num1 - num2;
+                break;
+            case '*':
+                ans = num1 * num2;
+                break;
+            default:
+                return 0;
+        }
         
         return ans;
     }
