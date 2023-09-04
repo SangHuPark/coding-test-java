7209cbd44df29f16b66551eacc8545f2224d844d
SangHuPark <tkdgn5408407@gmail.com>
2023-09-04 20:50:36 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 9.73 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        arr = Arrays.stream(arr).distinct().toArray();
+        int[] ans = new int[k];
+        Arrays.fill(ans, -1);
+        int count = 0;
+        
+        for (int num : arr) {
+            if (count == k)
+                break;
+            
+            ans[count++] = num;
+        }
+            
+        
+        return ans;
+    }
+}
