8f7e25290211216bda63100ec29eb52501d94ad0
SangHuPark <tkdgn5408407@gmail.com>
2023-09-05 14:46:38 +0900

    [unrated] Title: 문자열 묶기, Time: 13.21 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] strArr) {
+        int[] arr = new int[strArr.length];
+        
+        for (int i = 0; i < strArr.length; i++) {
+            arr[i] = strArr[i].length();
+        }
+        
+        int max = Arrays.stream(arr).max().getAsInt();
+        int[] countArr = new int[max];
+        
+        for (int num : arr) {
+            countArr[num-1]++;
+        }
+        
+        int answer = Arrays.stream(countArr).max().getAsInt();
+        return answer;
+    }
+}
