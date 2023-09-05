e4ddd4b3f5174bd38e147444f889c65193b116a5
SangHuPark <tkdgn5408407@gmail.com>
2023-09-05 14:57:15 +0900

    [unrated] Title: 문자열 묶기, Time: 7.97 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/README.md
+++ 프로그래머스/unrated/181855. 문자열 묶기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 문자열 묶기 - 181855 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181855) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181855?language=java) 
 
 ### 성능 요약
 
-메모리: 116 MB, 시간: 13.21 ms
+메모리: 119 MB, 시간: 7.97 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
@@ -2,20 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] strArr) {
-        int[] arr = new int[strArr.length];
+//         int[] arr = new int[strArr.length];
         
-        for (int i = 0; i < strArr.length; i++) {
-            arr[i] = strArr[i].length();
+//         for (int i = 0; i < strArr.length; i++) {
+//             arr[i] = strArr[i].length();
+//         }
+        
+//         int max = Arrays.stream(arr).max().getAsInt();
+//         int[] countArr = new int[max];
+        
+//         for (int num : arr) {
+//             countArr[num-1]++;
+//         }
+        
+        int[] countArr = new int[31];
+
+        for(int i=0; i < strArr.length; i++) {
+            countArr[strArr[i].length()]++;
         }
-        
-        int max = Arrays.stream(arr).max().getAsInt();
-        int[] countArr = new int[max];
-        
-        for (int num : arr) {
-            countArr[num-1]++;
-        }
-        
+
         int answer = Arrays.stream(countArr).max().getAsInt();
+        
         return answer;
     }
 }
