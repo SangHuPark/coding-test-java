91c3c425556b4d73b9793142a76e12a6fa8d547a
SangHuPark <tkdgn5408407@gmail.com>
2023-09-06 18:36:29 +0900

    [unrated] Title: 두 수의 합, Time: 9.14 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181846. 두 수의 합/README.md
+++ 프로그래머스/unrated/181846. 두 수의 합/README.md
@@ -1,10 +1,10 @@
 # [unrated] 두 수의 합 - 181846 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181846) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181846?language=java) 
 
 ### 성능 요약
 
-메모리: 104 MB, 시간: 228.13 ms
+메모리: 79.2 MB, 시간: 9.14 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.java
@@ -2,10 +2,24 @@ import java.math.BigInteger;
 
 class Solution {
     public String solution(String a, String b) {
-        BigInteger num1 = new BigInteger(a);
-        BigInteger num2 = new BigInteger(b);
+//         BigInteger num1 = new BigInteger(a);
+//         BigInteger num2 = new BigInteger(b);
         
-        num1 = num1.add(num2);
-        return num1.toString();
+//         num1 = num1.add(num2);
+//         return num1.toString();
+        
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1, j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            sb.append(sum % 10);
+            carry = sum / 10;
+        }
+        return sb.reverse().toString();
     }
 }
