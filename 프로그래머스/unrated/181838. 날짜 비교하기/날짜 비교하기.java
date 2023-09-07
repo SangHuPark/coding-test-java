bcffb49a828e8a176025db119d7f87104d6100d7
SangHuPark <tkdgn5408407@gmail.com>
2023-09-07 16:58:30 +0900

    [unrated] Title: 날짜 비교하기, Time: 1.74 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        String str1 = new String();
+        String str2 = new String();
+        
+        for (int i = 0; i < 3; i++) {
+            str1 += String.valueOf(date1[i]);
+            str2 += String.valueOf(date2[i]);
+        }
+        
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+        
+        return num1 < num2 ? 1 : 0;
+    }
+}
