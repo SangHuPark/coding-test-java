64b62d2fc91830c4fd9376aac859b34bf2c577bb
SangHuPark <tkdgn5408407@gmail.com>
2023-09-09 15:24:50 +0900

    [unrated] Title: l로 만들기, Time: 602.88 ms, Memory: 382 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        char[] arr = myString.toCharArray();
+        
+        for (char ch : arr) {
+            if (ch < 108)
+                ch = 'l';
+            answer += Character.toString(ch);
+        }
+        
+        return answer;
+    }
+}
