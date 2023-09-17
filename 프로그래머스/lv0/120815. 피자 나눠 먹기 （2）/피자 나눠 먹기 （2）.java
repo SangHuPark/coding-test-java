40abcde66fb1e2a6a596839b30893b8f3bf10deb
SangHuPark <tkdgn5408407@gmail.com>
2023-09-17 14:23:06 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,12 +1,19 @@
 class Solution {
     public int solution(int n) {
+//         for (int i = 1; i < n + 6; i++) {
+//             if ((n * i) % 6 == 0) {
+//                 return n * i / 6;
+//             }
+//         }
         
-        for (int i = 1; i < n + 6; i++) {
-            if ((n * i) % 6 == 0) {
-                return n * i / 6;
-            }
+//         return 0;
+        int answer = 1;
+
+        while(true){
+            if(6*answer%n==0) break;
+            answer++;
         }
-        
-        return 0;
+
+        return answer;
     }
 }
