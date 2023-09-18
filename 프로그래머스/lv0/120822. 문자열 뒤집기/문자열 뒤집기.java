34a55f367e058a8dedcf36431dfc19b23cede245
SangHuPark <tkdgn5408407@gmail.com>
2023-09-18 18:31:34 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.10 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,7 +1,9 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuffer sb = new StringBuffer(my_string);
-        String answer = sb.reverse().toString();
-        return answer;
+        // StringBuffer sb = new StringBuffer(my_string);
+        // String answer = sb.reverse().toString();
+        // return answer;
+        
+        return new StringBuffer(my_string).reverse().toString();
     }
 }
