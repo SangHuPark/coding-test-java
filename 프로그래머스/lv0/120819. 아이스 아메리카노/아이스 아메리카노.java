adf4f04e3c4568ba8e6cc4aedcea913f3d5ee6c6
SangHuPark <tkdgn5408407@gmail.com>
2023-09-18 18:16:44 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[] solution(int money) {
-        int count = 0;
-        int change = money % 5500;
-        while (money >= 5500) {
-            money -= 5500;
-            count++;
-            change = money % 5500;
-        }
+//         int count = 0;
+//         int change = money % 5500;
+//         while (money >= 5500) {
+//             money -= 5500;
+//             count++;
+//             change = money % 5500;
+//         }
         
-        int[] answer = {count, change};
+//         int[] answer = {count, change};
         
-        return answer;
+//         return answer;
+        
+        return new int[] { money / 5500, money % 5500 };
     }
 }
