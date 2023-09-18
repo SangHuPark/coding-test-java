0c643c0e7cc03e907e16392446b6f6186cb3011d
SangHuPark <tkdgn5408407@gmail.com>
2023-09-18 18:15:30 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int money) {
+        int count = 0;
+        int change = money % 5500;
+        while (money >= 5500) {
+            money -= 5500;
+            count++;
+            change = money % 5500;
+        }
+        
+        int[] answer = {count, change};
+        
+        return answer;
+    }
+}
