e07a559924acefa210308a67485532af83a6815d
SangHuPark <tkdgn5408407@gmail.com>
2023-09-25 20:14:31 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.47 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -2,8 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        Integer[] answer = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
-        Arrays.sort(answer, Collections.reverseOrder());
-        return answer[0] * answer[1];
+        // Integer[] answer = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+        // Arrays.sort(answer, Collections.reverseOrder());
+        // return answer[0] * answer[1];
+        
+        int leng = numbers.length - 1;
+        Arrays.sort(numbers);
+        return numbers[leng] * numbers[leng - 1];
     }
 }
