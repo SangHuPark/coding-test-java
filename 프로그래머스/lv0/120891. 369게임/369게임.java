468915137567152f8a56d8efde99882099856202
SangHuPark <tkdgn5408407@gmail.com>
2023-09-29 23:20:47 +0900

    [level 0] Title: 369게임, Time: 6.95 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.java
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -1,7 +1,8 @@
 class Solution {
     public int solution(int order) {
         int result = 0;
-        String tmp = new StringBuilder().append(order).toString();
+        // String tmp = new StringBuilder().append(order).toString();
+        String tmp = order + "";
         
         for (char ch : tmp.toCharArray()) {
             if (ch == '3' || ch == '6' || ch == '9')
