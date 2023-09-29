1d3dbc2016b1f776ce7e6b62d85d0eff0b78f20a
SangHuPark <tkdgn5408407@gmail.com>
2023-09-29 23:26:12 +0900

    [level 0] Title: 암호 해독, Time: 1.60 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -1,12 +1,16 @@
 class Solution {
     public String solution(String cipher, int code) {
         String result = "";
-        String[] arr = cipher.split("");
+//         String[] arr = cipher.split("");
         
-        for (int i = 0; i < arr.length; i++) {
-            if ((i+1) % code == 0) {
-                result += arr[i];
-            }
+//         for (int i = 0; i < arr.length; i++) {
+//             if ((i+1) % code == 0) {
+//                 result += arr[i];
+//             }
+//         }
+        
+        for (int i = code-1; i < cipher.length(); i += code) {
+            result += cipher.substring(i, i+1);
         }
         
         return result;
