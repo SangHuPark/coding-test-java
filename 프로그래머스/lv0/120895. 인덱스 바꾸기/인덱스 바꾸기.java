923470833c34072839ea0fef3ffad9fa7f54ced2
SangHuPark <tkdgn5408407@gmail.com>
2023-09-30 23:16:43 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.16 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] answer = my_string.split("");
+        String cup = "";
+        cup = answer[num1];
+        answer[num1] = answer[num2];
+        answer[num2] = cup;
+        
+        return String.join("", answer);
+    }
+}
