d9043e2f0a17f46d55193d1a6b6f3d85a2e970cd
SangHuPark <tkdgn5408407@gmail.com>
2023-10-03 17:59:04 +0900

    [level 0] Title: 숫자 찾기, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,13 +1,19 @@
 class Solution {
     public int solution(int num, int k) {
-        String[] strArr = (num + "").split("");
+//         String[] strArr = (num + "").split("");
         
-        for (int i = 0; i < strArr.length; i++) {
-            if (Integer.parseInt(strArr[i]) == k) {
-                return i+1;
-            }
-        }
+//         for (int i = 0; i < strArr.length; i++) {
+//             if (Integer.parseInt(strArr[i]) == k) {
+//                 return i+1;
+//             }
+//         }
         
-        return -1;
+//         return -1;
+        
+        String numStr = String.valueOf(num);
+        String kStr = String.valueOf(k);
+
+        int answer = numStr.indexOf(kStr);
+        return answer < 0 ? -1 : answer + 1;
     }
 }
