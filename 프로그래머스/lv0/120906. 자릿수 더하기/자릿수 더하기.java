be9b5e47ab492cc166aca21f6266c7c7fe1cac59
SangHuPark <tkdgn5408407@gmail.com>
2023-10-04 11:02:23 +0900

    [level 0] Title: 자릿수 더하기, Time: 2.17 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,12 +1,16 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        String strN = String.valueOf(n);
+//         int answer = 0;
+//         String strN = String.valueOf(n);
         
-        for (char ch : strN.toCharArray()) {
-            answer += ch - '0';
-        }
+//         for (char ch : strN.toCharArray()) {
+//             answer += ch - '0';
+//         }
         
-        return answer;
+//         return answer;
+        return Arrays.stream(String.valueOf(n).split(""))
+            .mapToInt(Integer::parseInt).sum();
     }
 }
