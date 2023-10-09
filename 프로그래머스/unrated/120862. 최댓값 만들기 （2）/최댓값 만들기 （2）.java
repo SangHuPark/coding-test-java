52bd8343a82527767f7b8089d5a77444af1443a9
SangHuPark <tkdgn5408407@gmail.com>
2023-10-09 15:46:51 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 0.35 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,14 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = -100000000;
+//         int answer = -100000000;
         
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = 0; j < numbers.length; j++) {
-                if (answer < numbers[i] * numbers[j] && i != j)
-                    answer = numbers[i] * numbers[j];
-            }
-        }
+//         for (int i = 0; i < numbers.length; i++) {
+//             for (int j = 0; j < numbers.length; j++) {
+//                 if (answer < numbers[i] * numbers[j] && i != j)
+//                     answer = numbers[i] * numbers[j];
+//             }
+//         }
         
-        return answer;
+//         return answer;
+        
+        int leng = numbers.length;
+        Arrays.sort(numbers);
+        return Math.max(numbers[0] * numbers[1], numbers[leng-2] * numbers[leng-1]);
     }
 }
