ef971f77d9c04a77ee4223cd5f02f9da75ef88a4
SangHuPark <tkdgn5408407@gmail.com>
2023-10-10 22:33:27 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.15 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        my_string = my_string.replaceAll("[A-z]", " ");
-        String[] strArr = my_string.split(" ");
+        String[] strArr = my_string.replaceAll("[A-z]", " ").split(" ");
         
         for (String str : strArr) {
             if (!str.equals("")) {
