9d7358f09e311fcd41b73668c116a1d4e99d13f5
SangHuPark <tkdgn5408407@gmail.com>
2023-10-13 16:07:04 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 0.09 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,28 +1,35 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1;
+        int answer = 0;
         
-        int cnt=1;
-        boolean check=false;
-        while(true){     
-            check=false;
+//         int cnt=1;
+//         boolean check=false;
+//         while(true){     
+//             check=false;
 
-            if(answer % 3 == 0){
-                answer++;
-                continue;
-            }    
+//             if(answer % 3 == 0){
+//                 answer++;
+//                 continue;
+//             }    
 
-            if(String.valueOf(answer).contains("3")){
-                answer++;
-                check = true;
-            }
+//             if(String.valueOf(answer).contains("3")){
+//                 answer++;
+//                 check = true;
+//             }
 
-            if(check) continue;
+//             if(check) continue;
 
-            if(cnt == n) break;
+//             if(cnt == n) break;
             
+//             answer++;
+//             cnt++;
+//         }
+        
+        for (int i = 1; i <= n; i++) {
             answer++;
-            cnt++;
+            if (answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+                i--;
+            }
         }
         
         return answer;
