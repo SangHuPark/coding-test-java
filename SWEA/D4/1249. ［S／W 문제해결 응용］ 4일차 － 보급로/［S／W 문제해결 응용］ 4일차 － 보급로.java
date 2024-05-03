cfe11faa58df83db1e125ef0eeb8f7d6fa011f08
SangHuPark <tkdgn5408407@gmail.com>
2024-05-04 00:32:46 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 143 ms, Memory: 29,352 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,84 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.util.PriorityQueue;
 
 /**
- * 1. 지도의 크기를 입력받는다.
- * 2. 땅의 정보를 입력받는다.
- * 3. 출발지(0,0)에서 도착지까지 BFS 진행
- * 	3-1. 진행하며 현재 map 에 숫자를 가지고 큐에 삽입
- *  3-2. 좌표 이동 시 누적 좌표값을 저장해 현재 방문하려는 위치의 값과 현재 누적값을 더했을 때 더 작을 경우 방문한다.
- *  3-3. 이미 최소값을 넘은 위치라면 종료
+ * 1. 지도의 크기
+ * 2. 2차원 배열 지도 정보
+ * 3. (0, 0) -> start, (mapSize-1, mapSize-1) definition
+ * 4. MAX 로 초기화한 2차원 배열
+ * 5. start 에서 이동하려는 좌표의 originMap 의 값을 더해 maxMap 의 값과 비교해 작으면 갱신 후 이동
+ *  5-1. 크면 큐에 삽입 x
  */
 public class Solution {
-    static class Tank {
+    static class Pos implements Comparable<Pos> {
         int row;
         int col;
-        int totalTime;
+        int cost;
 
-        public Tank(int row, int col, int totalTime) {
+        public Pos(int row, int col, int cost) {
             this.row = row;
             this.col = col;
-            this.totalTime = totalTime;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Pos o) {
+            return Integer.compare(this.cost, o.cost);
         }
     }
 
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static StringBuilder sb = new StringBuilder();
-    public static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
 
-    public static int mapSize;
-    public static int[][] map;
-    public static int[][] minTimeMap;
+    static int mapSize;
+    static int[][] originMap;
+    static int[][] maxMap;
 
-    public static Deque<Tank> tankQ;
+    static PriorityQueue<Pos> queue;
 
-    public static final int DELTA_LENG = 4;
-    public static int[] deltaRow = {0, 1, 0, -1};
-    public static int[] deltaCol = {1, 0, -1, 0};
+    static final int[] deltaRow = {0, 1, 0, -1};
+    static final int[] deltaCol = {1, 0, -1, 0};
 
-    public static void makeLoad(Tank startTank) {
-        tankQ = new ArrayDeque<>();
+    public static void find() {
+        queue = new PriorityQueue<>();
 
-        tankQ.add(startTank);
+        queue.offer(new Pos(0, 0, 0));
 
-        while(!tankQ.isEmpty()) {
-            Tank curTank = tankQ.poll();
+        while(!queue.isEmpty()) {
+            Pos pos = queue.poll();
 
-            // 도착지라면 갱신
-            if(curTank.row == mapSize-1 && curTank.col == mapSize-1) {
-                minTimeMap[mapSize - 1][mapSize - 1] = Math.min(minTimeMap[mapSize - 1][mapSize - 1], curTank.totalTime);
+            if (pos.row == mapSize-1 && pos.col == mapSize-1) {
                 continue;
             }
 
-            // 맵에 음수는 없기 때문에 이미 도착지보다 값이 크다면 검사할 필요가 없으므로 패스
-            if(curTank.totalTime > minTimeMap[mapSize-1][mapSize-1]) {
-                continue;
-            }
+            for (int deltaIdx = 0; deltaIdx < 4; deltaIdx++) {
+                int nextRow = pos.row + deltaRow[deltaIdx];
+                int nextCol = pos.col + deltaCol[deltaIdx];
 
-            for(int deltaIdx = 0; deltaIdx < DELTA_LENG; deltaIdx++) {
-                int nextRow = curTank.row + deltaRow[deltaIdx];
-                int nextCol = curTank.col + deltaCol[deltaIdx];
-
-                // 인덱스를 벗어나면 패스
-                if(isArrange(nextRow, nextCol)) {
+                if (isArrange(nextRow, nextCol))
                     continue;
-                }
 
-                // 다음 위치의 누적합 값이 현재 가진 값 + 기존의 다음 위치 값보다 작다면 패스
-                if(minTimeMap[nextRow][nextCol] <= curTank.totalTime + map[nextRow][nextCol]) {
+                if (pos.cost + originMap[nextRow][nextCol] >= maxMap[nextRow][nextCol])
                     continue;
-                }
 
-                // 다음 위치의 누적합 보다 작다면 갱신 후 이동
-                tankQ.add(new Tank(nextRow, nextCol, curTank.totalTime + map[nextRow][nextCol]));
-                minTimeMap[nextRow][nextCol] = curTank.totalTime + map[nextRow][nextCol];
+                queue.add(new Pos(nextRow, nextCol, pos.cost + originMap[nextRow][nextCol]));
+                maxMap[nextRow][nextCol] = pos.cost + originMap[nextRow][nextCol];
             }
         }
-
     }
 
     public static boolean isArrange(int row, int col) {
@@ -88,28 +76,27 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         int testCase = Integer.parseInt(br.readLine().trim());
 
-        for(int tc = 1; tc <= testCase; tc++) {
-            input();
+        for (int tc = 1; tc <= testCase; tc++) {
+            init();
 
-            makeLoad(new Tank(0, 0, 0));
+            find();
 
-            sb.append("#").append(tc).append(" ").append(minTimeMap[mapSize-1][mapSize-1]).append("\n");
+            sb.append("#").append(tc).append(" ").append(maxMap[mapSize - 1][mapSize - 1]).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    public static void input() throws IOException {
+    public static void init() throws IOException {
         mapSize = Integer.parseInt(br.readLine().trim());
 
-        map = new int[mapSize][mapSize];
-        minTimeMap = new int[mapSize][mapSize];
-        for(int row = 0; row < mapSize; row++) {
-            String st = br.readLine().trim();
-            for(int col = 0; col < mapSize; col++) {
-                map[row][col] = st.charAt(col) - '0';
-                // 최소값 갱신을 위해 max 로 초기화
-                minTimeMap[row][col] = Integer.MAX_VALUE;
+        originMap = new int[mapSize][mapSize];
+        maxMap = new int[mapSize][mapSize];
+        for (int row = 0; row < mapSize; row++) {
+            String input = br.readLine().trim();
+            for (int col = 0; col < mapSize; col++) {
+                originMap[row][col] = input.charAt(col) - '0';
+                maxMap[row][col] = Integer.MAX_VALUE;
             }
         }
     }
