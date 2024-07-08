73972ddaeb80904a737dfd355187221ebf19463f
SangHuPark <tkdgn5408407@gmail.com>
2024-07-08 22:42:24 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int input = Integer.parseInt(br.readLine());
+        
+        System.out.println(input - 543);
+    }
+}
