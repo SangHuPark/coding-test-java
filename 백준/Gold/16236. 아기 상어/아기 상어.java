2908f91c9573fc010c0d9fa4e8643e193a7bf6e9
SangHuPark <tkdgn5408407@gmail.com>
2024-11-13 22:43:18 +0900

    [Gold III] Title: 아기 상어, Time: 72 ms, Memory: 12012 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,115 +1,141 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, sharkRow, sharkCol, sharkSize = 2, time, eatCount = 0;
-    static int[][] map;
-    static int[] dR = {1, -1, 0, 0};
-    static int[] dC = {0, 0, 1, -1};
+    static class Pos {
+        int row;
+        int col;
+        int time;
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-
-        for(int i = 0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            for(int j = 0; j<N; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-
-                if(map[i][j] == 9){
-                    sharkRow = i;
-                    sharkCol = j;
-
-                    map[i][j] = 0;
-                }
-            }
+        public Pos(int row, int col, int time) {
+            this.row = row;
+            this.col = col;
+            this.time = time;
         }
-
-        int timeSum = 0;
-        //부모가 옮기지 않아도 될 때 계속 반복
-        while(sharkGo())
-            timeSum += time;
-
-        System.out.println(timeSum);
     }
 
-    static boolean sharkGo(){
-        time = 0;
-        //먹은 물고기의 수가 몸의 크기와 같아지면 몸의 크기 증가
-        if(eatCount == sharkSize){
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int mapSize;
+    static int[][] map;
+    static final int START = 9;
+
+    static int sharkSize, sharkRow, sharkCol, eatCount, resultTime;
+    static Deque<Pos> q;
+
+    static final int[] deltaRow = {0, 1, 0, -1};
+    static final int[] deltaCol = {1, 0, -1, 0};
+
+    // 부모를 사용하지 않고 가능한 경우를 수행
+    public static boolean eatFish() {
+        resultTime = 0;
+
+        // 사이즈만큼 먹으면 먹은 수 초기화 후 사이즈 증가
+        if (eatCount == sharkSize) {
             eatCount = 0;
             sharkSize++;
         }
-
-        boolean[][] visited = new boolean[N][N];
-
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(sharkRow, sharkCol, 0));
-        visited[sharkRow][sharkCol] = true;
 
         int minRow = Integer.MAX_VALUE;
         int minCol = Integer.MAX_VALUE;
         int minTime = Integer.MAX_VALUE;
 
-        while(!q.isEmpty()){
-            Node a = q.poll();
-            //최소 시간으로 물고기를 먹을 수 있는 시간을 넘으면 종료
-            if(a.time >= minTime)
+        q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[mapSize][mapSize];
+
+        q.add(new Pos(sharkRow, sharkCol, 0));
+        visited[sharkRow][sharkCol] = true;
+
+        while (!q.isEmpty()) {
+            Pos curPos = q.poll();
+
+            if (curPos.time >= minTime)
                 break;
 
-            for(int i = 0; i<4; i++){
-                int dr = a.row + dR[i];
-                int dc = a.col + dC[i];
+            for (int deltaIdx = 0; deltaIdx < 4; deltaIdx++) {
+                int nextRow = curPos.row + deltaRow[deltaIdx];
+                int nextCol = curPos.col + deltaCol[deltaIdx];
 
-                if(dr<0||dc<0||dr>=N||dc>=N)
-                    continue;
-                if(visited[dr][dc])
-                    continue;
-                if(map[dr][dc] > sharkSize)
+                if (isArrange(nextRow, nextCol) || visited[nextRow][nextCol])
                     continue;
 
-                //아기 상어가 먹을 수 있는 물고기가 있는 칸에 들어옴
-                if(map[dr][dc]>0 && map[dr][dc]<sharkSize){
-                    if(dr<minRow){
-                        minRow = dr;
-                        minCol = dc;
-                        minTime = a.time + 1;
-                    }
-                    else if(dr == minRow){
-                        if(dc < minCol){
-                            minCol = dc;
-                            minTime = a.time + 1;
+                // 더 큰 사이즈면 패스
+                if (map[nextRow][nextCol] > sharkSize)
+                    continue;
+
+                // 빈 공간이 아니면 더 작은 생선이므로 갱신
+                if (map[nextRow][nextCol] > 0 && map[nextRow][nextCol] < sharkSize) {
+                    // 가장 위에 있는 생선을 찾기 위해 이동 후 시간 증가
+                    if (nextRow < minRow) {
+                        minRow = nextRow;
+                        minCol = nextCol;
+                        minTime = curPos.time + 1;
+                    } // row 가 같으면 왼쪽 생선으로 이동
+                    else if (nextRow == minRow) {
+                        if (nextCol < minCol) {
+                            minCol = nextCol;
+                            minTime = curPos.time + 1;
                         }
                     }
                 }
 
-                q.offer(new Node(dr, dc, a.time+1));
-                visited[dr][dc] = true;
+                // 빈 공간이면 그냥 이동
+                q.offer(new Pos(nextRow, nextCol, curPos.time + 1));
+                visited[nextRow][nextCol] = true;
             }
+
         }
 
-        if(minTime == Integer.MAX_VALUE)
+        if (minTime == Integer.MAX_VALUE) {
             return false;
+        } // 가장 먼저 먹어야 하는 생선으로 이동 후 걸린 시간 갱신 및 먹었음 처리
         else {
             sharkRow = minRow;
             sharkCol = minCol;
             eatCount++;
-            time = minTime;
+            resultTime = minTime;
             map[sharkRow][sharkCol] = 0;
 
             return true;
         }
-    }
-}
-class Node{
-    int row, col, time;
 
-    Node(int row, int col, int time){
-        this.row = row;
-        this.col = col;
-        this.time = time;
     }
+
+    public static boolean isArrange(int row, int col) {
+        return row < 0 || col < 0 || row >= mapSize || col >= mapSize;
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        int totalTime = 0;
+        while (eatFish()) {
+            totalTime += resultTime;
+        }
+        System.out.println(totalTime);
+    }
+
+    public static void init() throws IOException {
+        mapSize = Integer.parseInt(br.readLine().trim());
+
+        map = new int[mapSize][mapSize];
+        for (int row = 0; row < mapSize; row++) {
+            st = new StringTokenizer(br.readLine().trim());
+            for (int col = 0; col < mapSize; col++) {
+                map[row][col] = Integer.parseInt(st.nextToken());
+
+                if (map[row][col] == START) {
+                    sharkRow = row;
+                    sharkCol = col;
+                    map[row][col] = 0;
+                }
+            }
+        }
+
+        sharkSize = 2;
+    }
+
 }
