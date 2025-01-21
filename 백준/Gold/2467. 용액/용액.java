025ce08eca53f7c03b12ae1691348e58cdb82410
SangHuPark <tkdgn5408407@gmail.com>
2025-01-21 19:30:23 +0900

    [Gold V] Title: 용액, Time: 240 ms, Memory: 31984 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -3,32 +3,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/**
- * 1. 용액 수 N 입력
- * 2. 용액의 특성값 배열 입력
- *  2-1. 특성값은 항상 오름차순으로 들어온다.
- * 3. lt = 0, rt = 1 에서 시작
- *  3-1. 현재 두 용액의 합 sum(|lt + rt| 에서 시작) 저장
- */
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
     static int N;
     static int[] numArr;
-    static int[] answers;
+    static int answerLeftIdx, answerRightIdx;
 
     public static void twoPointer() {
-        long min = Long.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
 
         int lt = 0, rt = N-1;
         while (lt < rt) {
-            long sum = numArr[lt] + numArr[rt];
+            int sum = numArr[lt] + numArr[rt];
 
-            long absSum = Math.abs(sum);
+            int absSum = Math.abs(sum);
             if (absSum < min) {
-                answers[0] = lt;
-                answers[1] = rt;
+                answerLeftIdx = lt;
+                answerRightIdx = rt;
                 min = absSum;
             }
 
@@ -38,7 +31,7 @@ public class Main {
                 lt++;
         }
 
-        System.out.println(numArr[answers[0]] + " " + numArr[answers[1]]);
+        System.out.println(numArr[answerLeftIdx] + " " + numArr[answerRightIdx]);
     }
 
     public static void main(String[] args) throws IOException {
@@ -56,8 +49,6 @@ public class Main {
         for (int idx = 0; idx < N; idx++) {
             numArr[idx] = Integer.parseInt(st.nextToken());
         }
-
-        answers = new int[2];
     }
 
 }
