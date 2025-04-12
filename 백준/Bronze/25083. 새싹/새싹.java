74854ec4dae31a03f7b3a924407151c0cd6f8fe1
SangHuPark <tkdgn5408407@gmail.com>
2025-04-12 21:09:30 +0900

    [Bronze V] Title: 새싹, Time: 64 ms, Memory: 11452 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    
+    public static void main(String[] args) throws IOException {
+        System.out.print("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+
+    public static void init() throws IOException {
+    }
+}
