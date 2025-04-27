9ddbf5f7c20cf5b36d6d0a7be5771a77744c9f2c
SangHuPark <tkdgn5408407@gmail.com>
2025-04-28 00:03:33 +0900

    [Bronze V] Title: 등록, Time: 64 ms, Memory: 11300 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("262\n" + "tkdgn407");
+    }
+
+}
