102036cfafab926399ff6f324796c62948de46d0
SangHuPark <tkdgn5408407@gmail.com>
2025-06-22 17:17:34 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 64 ms, Memory: 11632 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * BOJ_1011_Fly me to the Alpha Centauri
+ * @author tkdgn407
+ *
+ * 1.
+ */
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static long x, y, d, k;
+
+    public static void main(String[] args) throws IOException {
+        int testCase = Integer.parseInt(br.readLine().trim());
+
+        for (int T = 0; T < testCase; T++) {
+            st = new StringTokenizer(br.readLine().trim());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+
+            d = y - x;
+            k = (long) Math.sqrt(d);
+
+            long answer = 0;
+            if (d <= k * k) answer = (k << 1) - 1;
+            else if (k * k < d && d <= k * (k + 1)) answer = k << 1;
+            else if (d > k * (k + 1)) answer = (k << 1) + 1;
+
+            sb.append(answer).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
