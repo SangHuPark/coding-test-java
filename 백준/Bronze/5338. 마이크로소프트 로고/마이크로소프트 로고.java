5440205c71c454393a70473ba0e6f5213a319eec
SangHuPark <tkdgn5408407@gmail.com>
2025-06-29 17:49:42 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 60 ms, Memory: 11416 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * BOJ_문제번호_문제명
+ * @author tkdgn407
+ *
+ * 1.
+ */
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int A, B;
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
