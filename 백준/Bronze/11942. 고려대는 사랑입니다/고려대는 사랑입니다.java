9a5b3467622411c2512b9026854ecaa997ed8695
SangHuPark <tkdgn5408407@gmail.com>
2025-07-15 23:35:06 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 60 ms, Memory: 11288 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * BOJ_문제번호_문제명
+ * @author tkdgn407
+ *
+ * 1.
+ */
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("고려대학교");
+    }
+}
