82632bd93d5f7cde0428cd058769c4ddcb18f770
SangHuPark <tkdgn5408407@gmail.com>
2025-07-26 13:31:47 +0900

    [Bronze V] Title: 웰컴, Time: 60 ms, Memory: 11224 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(String.valueOf(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/."));
+    }
+}
