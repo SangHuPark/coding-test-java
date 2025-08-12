ab11faa48379b1997a40e5a58664cfb46be98bab
SangHuPark <tkdgn5408407@gmail.com>
2025-08-12 23:07:06 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.06 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder answer = new StringBuilder(my_string);
+        
+        answer.reverse();
+        
+        return answer.toString();
+    }
+}
