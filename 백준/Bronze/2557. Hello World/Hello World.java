2984154e2175299c8c4d96882e5dbbf8e130109b
SangHuPark <tkdgn5408407@gmail.com>
2025-08-14 10:33:00 +0900

    [Bronze V] Title: Hello World, Time: 60 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * BOJ_문제번호_문제명
+ * @author
+ *
+ * 1.
+ */
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        sb.append("Hello World!");
+        bw.write(sb.toString());
+        bw.close();
+    }
+}
