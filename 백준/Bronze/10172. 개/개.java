a7f758e243e4db2e0e72898a5da06a6963e1d6fa
SangHuPark <tkdgn5408407@gmail.com>
2025-08-14 10:40:12 +0900

    [Bronze V] Title: 개, Time: 64 ms, Memory: 11552 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,16 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+/**
+ * BOJ_문제번호_문제명
+ * @author
+ *
+ * 1.
+ */
 public class Main {
+    static StringBuilder sb = new StringBuilder();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        System.out.println("|\\_/|\n" +
+
+        sb.append("|\\_/|\n" +
                 "|q p|   /}\n" +
                 "( 0 )\"\"\"\\\n" +
                 "|\"^\"`    |\n" +
                 "||_/=\\\\__|");
+        bw.write(sb.toString());
+        bw.close();
     }
 }
