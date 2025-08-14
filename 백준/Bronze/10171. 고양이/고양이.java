10a628a5115c792a67162c310e0b44888dbe6e58
SangHuPark <tkdgn5408407@gmail.com>
2025-08-14 10:39:36 +0900

    [Bronze V] Title: 고양이, Time: 60 ms, Memory: 11372 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * BOJ_문제번호_문제명
+ * @author
+ *
+ * 1.
+ */
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        sb.append("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+        bw.write(sb.toString());
+        bw.close();
+    }
+}
