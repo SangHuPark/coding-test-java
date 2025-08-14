e94fca9bc5df8ec06155e6b7e4d7c00186fa5b77
SangHuPark <tkdgn5408407@gmail.com>
2025-08-14 10:21:46 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 64 ms, Memory: 11620 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * BOJ_문제번호_문제명
+ * @author
+ *
+ * 1.
+ */
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine().trim());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        if (A < B)
+            sb.append("<");
+        else if (A > B)
+            sb.append(">");
+        else
+            sb.append("==");
+
+        bw.write(sb.toString());
+        bw.close();
+    }
+}
