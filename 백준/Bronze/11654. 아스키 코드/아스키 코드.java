3a89ccb159334538cbe9db25bf0059878ee0212e
SangHuPark <tkdgn5408407@gmail.com>
2026-03-04 11:23:37 +0900

    [Bronze V] Title: 아스키 코드, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,12 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+/**
+ * BOJ_문제번호_문제명
+ * @author
+ *
+ * 1.
+ */
 public class Main {
+    static StringBuilder sb = new StringBuilder();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        System.out.println((int) br.readLine().trim().charAt(0));
+        int A = br.readLine().trim().charAt(0);
+        System.out.println(A);
     }
 }
